81585893f0c0601c004ffcb2d6dcf1551f57537b
arjun963 <arjun.bhattrai.7@gmail.com>
2024-09-03 18:57:23 +0530

    navbar

--- src/components/Shared/Navbar.js
+++ src/components/Shared/Navbar.js
@@ -7,25 +7,45 @@ const Navbar = () => {
 	const { isLoggedIn, logout } = useContext(AuthContext);
 
 	return (
-		<AppBar position="static">
+		<AppBar position="static" sx={{ backgroundColor: "#282c34" }}>
+			{" "}
+			{/* Set background color */}
 			<Toolbar>
-				<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-					<Link to="/" className="text-white no-underline">
+				<Typography
+					variant="h6"
+					component="div"
+					sx={{ flexGrow: 1, color: "#FFA500" }}
+				>
+					{" "}
+					{/* Set text color */}
+					<Link
+						to="/"
+						className="no-underline mr-4"
+						style={{ color: "#008000" }}
+					>
 						Quiz App
 					</Link>
 				</Typography>
 				{isLoggedIn ? (
 					<>
-						<Link to="/admin" className="text-white no-underline mr-4">
-							<Button color="inherit">Admin</Button>
+						<Link to="/admin" className="no-underline mr-4">
+							<Button color="inherit" sx={{ color: "#FFC594" }}>
+								Admin
+							</Button>{" "}
+							{/* Set button text color */}
 						</Link>
-						<Button color="inherit" onClick={logout}>
+						<Button color="inherit" onClick={logout} sx={{ color: "#FFA500" }}>
+							{" "}
+							{/* Set button text color */}
 							Logout
 						</Button>
 					</>
 				) : (
-					<Link to="/admin/login" className="text-white no-underline">
-						<Button color="inherit">Login</Button>
+					<Link to="/admin/login" className="no-underline">
+						<Button color="inherit" sx={{ color: "#FFA500" }}>
+							Login
+						</Button>{" "}
+						{/* Set button text color */}
 					</Link>
 				)}
 			</Toolbar>
